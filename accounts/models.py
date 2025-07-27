8964f5dba2aa3510813ceb58ab7f252b055ab6b4
nimamze <nimamze3@gmail.com>
2025-07-27 16:45:42 +0330

    add : custom user manager and model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self,phone,email,password,**kwargs):
+
+        email = self.normalize_email(email)
+        user = self.model(phone = phone,**kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self,phone,email,password,**kwargs):
+
+        kwargs.setdefault("is_staff", True) 
+        kwargs.setdefault("is_superuser", True)
+        kwargs.setdefault("is_active", True)
+        return self.create_user(phone,email, password, **kwargs)  
+    
+    
+class CustomUser(AbstractUser):
+
+    username = None
+    phone = models.CharField(unique=True,max_length=15)
+    email = models.EmailField()
+    image = models.ImageField(null=True,blank=True)
+    date = models.DateField(auto_now_add=True)
+    USERNAME_FIELD = 'phone'
+    objects = CustomUserManager() # type: ignore

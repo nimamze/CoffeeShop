4657df536963a5f0919eb85ce30cdb758613a8e3
Iliya-Sedghi <iliwsjoe1902@gmail.com>
2025-07-28 15:24:08 +0330

    add: simple form, url, view, for product image model

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import ProductImageView
 
 urlpatterns = [
-    
+    path('product-images/',ProductImageView.as_view(), name = 'product-image')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import ProductImage
+from .forms import ProductImageForm
 
 # Create your views here.
+class ProductImageView(CreateView):
+    model = ProductImage
+    form_class = ProductImageForm
+    template_name = 'product_image_form.html'
+    success_url = reverse_lazy('home')

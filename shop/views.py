39b2d6e648fd86e49024a54f81b76d392f578b78
Iliya-Sedghi <iliwsjoe1902@gmail.com>
2025-07-29 15:35:26 +0330

    modify: product list to have category filter

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
-from .models import ProductImage, Product
+from .models import ProductImage, Product, Category
 from .forms import ProductImageForm
 
 # Create your views here.
@@ -16,6 +16,18 @@ class ProductListView(ListView):
     model = Product
     template_name = 'products_list.html'
     context_object_name = 'products'
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        category_id = self.request.GET.get('category')
+        if category_id:
+            queryset = queryset.filter(category__id=category_id)
+        return queryset.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['selected_category'] = self.request.GET.get('category')
+        return context
 
 class ProductDetailView(DetailView):
     model = Product

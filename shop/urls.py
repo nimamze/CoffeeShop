55867077b035b6e5b9e57a6bfe7bf0affa35901f
amirriazizadeh <ariazizade@gmail.com>
2025-08-03 20:36:32 +0330

    a litle debug

--- shop/urls.py
+++ shop/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from accounts.views import ProfileView,UpdateProfileView,UserFavoritesView,add_to_favorites,UserOrdersView,UserOrderDetailView
-from shop.views import ProductList,ProductDetailView
+from .views import ProductList,ProductDetailView,CartAddView
 
 urlpatterns = [
     path('',ProductList.as_view(),name='product_list'),
     path('category/<str:name>',ProductList.as_view(),name='category_filter'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_details'),
-    path('addFavorite/',add_to_favorites,name='add_to_favorites')
+    path('addFavorite/',add_to_favorites,name='add_to_favorites'),
+    path('cart/add/<int:product_id>/', CartAddView.as_view(), name='cart_add'),
 ]

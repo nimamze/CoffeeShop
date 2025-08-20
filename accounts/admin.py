cf6eaafa86e0712a7861e0e77c26302476f16ef6
nimamze <nimamze3@gmail.com>
2025-08-20 15:56:04 +0330

    Fix Users Part In Admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,9 +6,24 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['phone', 'email', 'first_name', 'last_name', 'is_staff']
+    list_display = ["phone", "email", "first_name", "last_name", "is_staff"]
     list_filter = ("is_staff", "is_active")
-    fieldsets = UserAdmin.fieldsets
-    add_fieldsets = UserAdmin.add_fieldsets
-    search_fields = ("username", "email")
+
+    fieldsets = (
+        (None, {"fields": ("phone", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email", "image")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("phone", "email", "password1", "password2"),
+            },
+        ),
+    )
+
+    search_fields = ("phone", "email")
     ordering = ["phone"]

f887b850eaa667a55c3bacac2f99b5b26c9bf2ab
nimamze <nimamze3@gmail.com>
2025-08-22 22:14:02 +0330

    Replace Product.objects.get() with get_object_404 Add raise exception for serializer.isvalid Add favorite.objects.get_or_create Add quantity when order Change the permission of image edit for only admin

--- shop/api/v1/views.py
+++ shop/api/v1/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
-from shop.models import Category, Product
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.db.models import Q
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import permission_classes
+from shop.models import Category, Product, Favorite, Cart, CartItem, ProductImage
 from .serializers import (
     CategorySerializer,
     ProductSerializer,
@@ -12,18 +15,16 @@ from .serializers import (
     ProductDetailPostSerializer,
     ProductImageSerializer,
 )
-from ...models import Product, Favorite, Cart, CartItem, ProductImage
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework.decorators import permission_classes
-from django.db.models import Q
 
 
 class ProductListApi(APIView):
     def get(self, request, name=None):
         categories = Category.objects.all()
         category_data = CategorySerializer(categories, many=True).data
+
         products = Product.objects.all()
         q = Q()
+
         if name:
             q |= Q(categories__name__icontains=name)
 
@@ -36,11 +37,14 @@ class ProductListApi(APIView):
         if search_terms:
             for term in search_terms:
                 q |= Q(name__icontains=term) | Q(description__icontains=term)
+
         if q:
             products = products.filter(q).distinct()
+
         paginator = PageNumberPagination()
         paginator.page_size = int(request.query_params.get("page_size", 5))
         result_page = paginator.paginate_queryset(products, request)
+
         product_data = ProductSerializer(result_page, many=True).data
         return paginator.get_paginated_response(
             {
@@ -52,7 +56,7 @@ class ProductListApi(APIView):
 
 class ProductDetailApi(APIView):
     def get(self, request, pk):
-        product = Product.objects.get(pk=pk)
+        product = get_object_or_404(Product, pk=pk)
         serializer = ProductDetailSerializer(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -60,33 +64,34 @@ class ProductDetailApi(APIView):
     @permission_classes([IsAuthenticated])
     def post(self, request, pk):
         serializer = ProductDetailPostSerializer(data=request.data)
-        if serializer.is_valid():
-            data = serializer.validated_data
-            fav = data.get("favorite")  # type: ignore
-            order_amount = data.get("order_amount")  # type: ignore
-            user = request.user
-            product = Product.objects.get(pk=pk)
-            if fav:
-                Favorite.objects.create(customer=user, product=product)
-            if order_amount and order_amount > 0 and product.availability:
-                product.stock -= order_amount
-                if product.stock < 0:
-                    return Response(
-                        {"message": "cant order more than the product stock"},
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
-                elif product.stock == 0:
-                    product.availability = False
-                cart = user.carts_customer.first()
-                if not cart:
-                    cart = Cart.objects.create(customer=user)
-                CartItem.objects.create(cart=cart, product=product)
-                product.save()
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        fav = data.get("favorite")  # type: ignore
+        order_amount = data.get("order_amount")  # type: ignore
+        user = request.user
+        product = get_object_or_404(Product, pk=pk)
+
+        if fav:
+            Favorite.objects.get_or_create(customer=user, product=product)
+
+        if order_amount and order_amount > 0 and product.availability:
+            if product.stock < order_amount:
+                return Response(
+                    {"message": "Cannot order more than product stock"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            product.stock -= order_amount
+            if product.stock == 0:
+                product.availability = False
+            cart, _ = Cart.objects.get_or_create(customer=user)
+            CartItem.objects.create(cart=cart, product=product, quantity=order_amount)
+            product.save()
+
         return Response(status=status.HTTP_200_OK)
 
 
 class ProductImageEditApi(APIView):
-    permission_classes = [IsAuthenticated, IsAdminUser]
+    permission_classes = [IsAdminUser]
 
     def get(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -97,20 +102,16 @@ class ProductImageEditApi(APIView):
     def post(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         serializer = ProductImageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(product=product)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(product=product)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def delete(self, request, pk):
-        image_id = request.data.get("id")
+        image_id = request.data.get("image_id")
         if not image_id:
             return Response(
                 {"error": "Image id is required"}, status=status.HTTP_400_BAD_REQUEST
             )
-
         image = get_object_or_404(ProductImage, pk=image_id, product_id=pk)
         image.delete()
-        return Response(
-            {"message": "Image deleted successfully"}, status=status.HTTP_204_NO_CONTENT
-        )
+        return Response(status=status.HTTP_204_NO_CONTENT)

d86cc92a6181958d7ade044741301439551963b3
Iliya-Sedghi <iliwsjoe1902@gmail.com>
2025-08-25 19:21:04 +0330

    add: blacklist after rotation in setting

--- coffeeShop/settings.py
+++ coffeeShop/settings.py
@@ -167,6 +167,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 SWAGGER_SETTINGS = {
